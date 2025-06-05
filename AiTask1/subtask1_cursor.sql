22d5e560e76284cd67b5467dc84480c7ded8a831
Ilya Kolesnikov <ilya.kolesnikov@innowise.com>
2025-06-05 12:13:51 +0200

    Include forgotten cursor code

--- AiTask1/subtask1_cursor.sql
+++ AiTask1/subtask1_cursor.sql
@@ -1,5 +1,4 @@
--- before
-
+-- Before
 SELECT
     o.order_id,
     o.customer_id,
@@ -22,4 +21,33 @@ LEFT JOIN currencies c
 WHERE o.created_at::date = CURRENT_DATE - 1
 GROUP BY
     o.order_id, o.customer_id, r.total_refund, c.iso_code
+ORDER BY gross_sales DESC;
+
+
+-- After
+WITH order_items_sums AS (
+    SELECT DISTINCT
+        order_id,
+        SUM(CASE WHEN status = 'FULFILLED' THEN quantity * unit_price ELSE 0 END) 
+            OVER (PARTITION BY order_id) AS gross_sales
+    FROM order_items
+),
+refund_sums AS (
+    SELECT DISTINCT
+        order_id,
+        SUM(amount) FILTER (WHERE created_at::date = CURRENT_DATE - 1)
+            OVER (PARTITION BY order_id) AS total_refund
+    FROM refunds
+)
+SELECT
+    o.order_id,
+    o.customer_id,
+    COALESCE(ois.gross_sales, 0) AS gross_sales,
+    COALESCE(rs.total_refund, 0) AS total_refund,
+    c.iso_code AS currency
+FROM orders o
+LEFT JOIN order_items_sums ois ON ois.order_id = o.order_id
+LEFT JOIN refund_sums rs ON rs.order_id = o.order_id
+LEFT JOIN currencies c ON c.currency_id = o.currency_id
+WHERE o.created_at::date = CURRENT_DATE - 1
 ORDER BY gross_sales DESC;
